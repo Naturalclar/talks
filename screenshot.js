39864804dd823ccd8ca8d8dbd958970c1db7ca38
Naturalclar <jesse.katsumata@gmail.com>
2019-07-24 02:10:03 +0900

    :sparkles: add screenshot script

---
+++ screenshot.js
@@ -0,0 +1,10 @@
+const puppeteer = require("puppeteer");
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto("http://localhost:8080");
+  await page.screenshot({ path: "example.png" });
+
+  await browser.close();
+})();
