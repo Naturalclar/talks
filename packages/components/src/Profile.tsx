991f4e5fb4f4155cf9b1167ad80e976bb6a76356
Naturalclar <jesse.katsumata@gmail.com>
2019-07-28 21:49:32 +0900

    :sparkles: update profile

--- packages/components/src/Profile.tsx
+++ packages/components/src/Profile.tsx
@@ -1,11 +1,25 @@
 import React from "react";
 
+const styles = {
+  line: {
+    height: 1,
+    width: "100%",
+    backgroundColor: "white"
+  },
+  list: {
+    listStyleType: "none"
+  }
+};
+
 const Profile = () => (
   <div>
-    <ul>
+    <div style={styles.line} />
+    <ul style={styles.list}>
       <li>Jesse Katsumata</li>
+      <li>CureApp, Inc.</li>
       <li>Node.js, React, TypeScript</li>
     </ul>
+    <div style={styles.line} />
   </div>
 );
 
