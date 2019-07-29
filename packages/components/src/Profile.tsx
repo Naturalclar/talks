75cf09b7290221c98c29f039d89f6411eb5cd21d
Naturalclar <jesse.katsumata@gmail.com>
2019-07-29 19:33:57 +0900

    :sparkles: add social linkts

--- packages/components/src/Profile.tsx
+++ packages/components/src/Profile.tsx
@@ -26,8 +26,14 @@ const Profile = () => (
     <div style={styles.line} />
     <ul style={styles.list}>
       <li style={styles.name}>Jesse Katsumata</li>
-      <li style={styles.company}>CureApp, Inc.</li>
+      <li style={styles.company}>Engineer at CureApp, Inc.</li>
       <li style={styles.listItemSub}>React, React-Native, TypeScript, Node</li>
+      <li style={styles.listItemSub}>
+        <a href="https://twitter.com/natural_clar">Twitter: @natural_clar</a>
+      </li>
+      <li style={styles.listItemSub}>
+        <a href="https://github.com/Naturalclar">Github: @Naturalclar</a>
+      </li>
     </ul>
     <div style={styles.line} />
   </div>
