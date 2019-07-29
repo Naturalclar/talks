c6fc74a18e55e52bb9943dcc496f21104b1e53e8
Naturalclar <jesse.katsumata@gmail.com>
2019-07-30 00:14:53 +0900

    :wrench: fix meta info

--- oembed.js
+++ oembed.js
@@ -1,15 +1,12 @@
-const title = process.argv.slice(2)[0];
+const [title] = process.argv.slice(2);
 const oEmbed = {
   type: "rich",
   version: "1.0",
   provider_name: `${title}.naturalclar.now.sh`,
   provider_url: `https://${title}.naturalclar.now.sh/`,
-  title: title,
+  title: "",
   width: 658,
   height: 408,
-  html:
-    '<iframe style="width: 100%; overflow: hidden;" src="https://' +
-    title +
-    '.naturalclar.now.sh/" width="658" height="408" frameborder="0" scrolling="no" ></iframe>'
+  html: `<iframe style="width: 100%; overflow: hidden;" src="https://${title}.naturalclar.now.sh/index.html" width="658" height="408" frameborder="0" scrolling="no" ></iframe>`
 };
 console.log(JSON.stringify(oEmbed));
--- packages/components/src/Meta.tsx
+++ packages/components/src/Meta.tsx
@@ -18,18 +18,19 @@ const Meta = ({
   host
 }: Props) => (
   <>
-    <meta name="twitter:description" content={description} />
-    <meta property="og:description" content={description} />
     <meta property="og:locale" content={locale} />
     <meta property="og:title" content={title} />
+    <meta property="og:description" content={description} />
     <meta property="og:type" content="article" />
     <meta property="og:url" content={host} />
     <meta property="og:image" content={`${host}/assets/card.png`} />
     <meta property="article:published_time" content={publishedAt} />
     <meta property="article:author" content="naturalclar" />
-    <meta name="twitter:card" content="summary" />
+    <meta name="twitter:card" content="summary_large_image" />
     <meta name="twitter:title" content={title} />
-    <meta name="twitter:image" content={`${host}/assets/card.png`} />
+    <meta name="twitter:image:src" content={`${host}/assets/card.png`} />
+    <meta name="twitter:description" content={description} />
+    <meta name="twitter:url" content={host} />
     <title>{title}</title>
     <link
       rel="alternate"
