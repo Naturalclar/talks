f92163014096f2027bcd7a07b319e8889f2d596d
Jesse Katsumata <jesse.katsumata@gmail.com>
2021-03-04 18:12:02 +0900

    feat: new slide

--- packages/components/src/Profile.tsx
+++ packages/components/src/Profile.tsx
@@ -5,27 +5,27 @@ const styles = {
   line: {
     height: 2,
     width: "100%",
-    backgroundColor: "white"
+    backgroundColor: "white",
   },
   list: {
-    listStyleType: "none"
+    listStyleType: "none",
   },
   name: {
-    fontSize: 30
+    fontSize: 30,
   },
   company: {
     fontSize: 24,
-    marginBottom: "8px"
+    marginBottom: "8px",
   },
 
   listItemSub: {
     fontSize: 20,
-    marginBottom: "8px"
+    marginBottom: "8px",
   },
   content: {
     display: "flex",
-    alignItems: "center"
-  }
+    alignItems: "center",
+  },
 };
 
 export const Profile = () => (
@@ -39,7 +39,7 @@ export const Profile = () => (
         <li style={styles.listItemSub}>Member of:</li>
         <li style={styles.listItemSub}>- React Native Community</li>
         <li style={styles.listItemSub}>- Reason React Native</li>
-        <li style={styles.listItemSub}>- Gatsby</li>
+        <li style={styles.listItemSub}>- React Native JP</li>
 
         <li style={styles.listItemSub}>
           <a href="https://twitter.com/natural_clar">Twitter: @natural_clar</a>
